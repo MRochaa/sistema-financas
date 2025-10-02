af5530e172d80d35de6953bfd4d82913b1f4b0ca
MRochaa <98289498+MRochaa@users.noreply.github.com>
2025-10-02 15:07:43 -0300

    Updated Transactions.tsx

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Plus, Edit, Trash2, Search, Filter } from 'lucide-react';
+import { Plus, CreditCard as Edit, Trash2, Search, Filter } from 'lucide-react';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { useData } from '../contexts/DataContext';
